70151f51cd9b31355ce145c85bffeb819ed14f89
Alex <alexrao20002@gmail.com>
2025-07-20 18:23:49 -0700

    Deleted Auth files (test)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Referral from './pages/Referral';
 import Register from './pages/Register';
-import Auth from './pages/Auth';
 import './App.css'
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/auth" element={<Auth />}/>
+      <Route path="/auth" element={<Register />}/>
       <Route path="/referral" element={<Referral />}/>
       <Route path="/register" element={<Register />}/>
     </Routes>
